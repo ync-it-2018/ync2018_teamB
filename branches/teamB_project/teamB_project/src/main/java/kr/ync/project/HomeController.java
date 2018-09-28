1756cdc46bc67e6e08cc7ea3c4f5ccd570bad882
team_b <team_b@c5a27e1c-843c-4872-b05a-80b93a2969c7>
2018-09-28 14:04:13 +0000

    branche 2

--- branches/teamB_project/teamB_project/src/main/java/kr/ync/project/HomeController.java
+++ branches/teamB_project/teamB_project/src/main/java/kr/ync/project/HomeController.java
@@ -25,7 +25,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 
-		logger.info("Welcome home! branche The client locale is {}.", locale);
+		logger.info("Welcome home! branche22 The client locale is {}.", locale);
 
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
