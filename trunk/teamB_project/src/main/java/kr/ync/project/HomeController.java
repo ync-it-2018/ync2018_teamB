d9ad100215ea356d0e2446caa9e7289f17cf06fb
team_b <team_b@c5a27e1c-843c-4872-b05a-80b93a2969c7>
2018-09-28 10:29:18 +0000

    text6666

--- trunk/teamB_project/src/main/java/kr/ync/project/HomeController.java
+++ trunk/teamB_project/src/main/java/kr/ync/project/HomeController.java
@@ -25,8 +25,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 
-		logger.info("Welcome home! 55555 The client locale is {}.", locale);
-
+		logger.info("Welcome home! 666666 The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
