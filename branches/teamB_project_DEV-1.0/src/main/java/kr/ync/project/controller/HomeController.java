378503f4dc27ebd16ecd01ccb5f506d8f91b23e9
team_b <team_b@c5a27e1c-843c-4872-b05a-80b93a2969c7>
2018-10-15 09:07:20 +0000

    #이건영 JSP변환2

--- branches/teamB_project_DEV-1.0/src/main/java/kr/ync/project/controller/HomeController.java
+++ branches/teamB_project_DEV-1.0/src/main/java/kr/ync/project/controller/HomeController.java
@@ -18,15 +18,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+	//커밋수정
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 
-		// 가나다
-		logger.info("teamB log 내용", locale);
+		// 媛��굹�떎
+		logger.info("teamB log �궡�슜", locale);
 
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
