a02370e32d9eb584504ff99f358c57dc4b9a6d1e
team_b <team_b@c5a27e1c-843c-4872-b05a-80b93a2969c7>
2018-12-08 12:55:01 +0000

    #이정욱 어드민 제품등록 기능추가

--- branches/teamB_project_DEV-1.0/src/main/java/kr/ync/project/controller/admin/Product_regiController.java
+++ branches/teamB_project_DEV-1.0/src/main/java/kr/ync/project/controller/admin/Product_regiController.java
@@ -12,15 +12,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import kr.ync.project.controller.HomeController;
 
 @Controller
-public class puroduct_regiController {
-private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+public class Product_regiController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
 	@RequestMapping(value = "/product_registration", method = RequestMethod.GET)
 	public String product_registration(Locale locale, Model model) {
 
 		logger.info("teamB log product_registration 페이지", locale);
 
 		return "admin/product_registration";
+
 	}
-	
 }
